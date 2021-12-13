0737bd9b964871308a160afe610a23a66163784d
África <afrirefran@gmail.com>
2021-12-13 19:41:41 +0100

    changes

--- src/app/components/home/movie/movie.component.ts
+++ src/app/components/home/movie/movie.component.ts
@@ -11,25 +11,25 @@ export class MovieComponent implements OnInit {
     {
       name: 'Spiderman',
       top: false,
-      description: 'El joven Petter Parker se convierte en un puto arácnido',
+      description: '',
       img: 'https://i1.wp.com/hipertextual.com/wp-content/uploads/2021/11/spider-man-sin-camino-a-casa.jpg?fit=1200%2C796&ssl=1'
     },
     {
       name: 'El señor de los anillos',
       top: false,
-      description: 'Un puto Hobbit se encuentra un anillo malvado',
+      description: '',
       img: 'https://as01.epimg.net/meristation/imagenes/2021/07/07/noticias/1625672419_027507_1625737804_noticia_normal.jpg'
     },
     {
-      name: 'Harry Cipoter',
+      name: 'Harry Potter',
       top: true,
-      description: 'Es un niño malcriado que se cae de una escalera y se hace una cicatriz brechosa en la frente y le provoca un tumor cerebral y se cree que es mago',
+      description: '',
       img: null,
     },
     {
-      name: 'Churrasic Park',
+      name: 'Jurasic Park',
       top: true,
-      description: 'un Churro de parque que acaba mal',
+      description: '',
       img: 'https://static.posters.cz/image/750/posters/jurassic-park-classic-logo-i57759.jpg'
     },
 ];
